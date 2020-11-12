5d6bd2ca16daa66e9bc7e76a555b996ee6c08fd6
Bladejs <JBladejs@gmail.com>
2020-11-12 18:16:51 +0100

    Conversion to kotlin

--- core/src/com/game_physics/collisions/CollisonsGame.kt
+++ core/src/com/game_physics/collisions/CollisonsGame.kt
@@ -1,33 +1,25 @@
-package com.game_physics.collisions;
+package com.game_physics.collisions
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-public class CollisonsGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+class CollisonsGame : ApplicationAdapter() {
+    var batch: SpriteBatch? = null
+    override fun create() {
+        batch = SpriteBatch()
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    override fun render() {
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch!!.begin()
+        batch!!.end()
+    }
+
+    override fun dispose() {
+        batch!!.dispose()
+    }
 }
--- desktop/src/com/game_physics/collisions/desktop/DesktopLauncher.kt
+++ desktop/src/com/game_physics/collisions/desktop/DesktopLauncher.kt
@@ -1,12 +1,13 @@
-package com.game_physics.collisions.desktop;
+package com.game_physics.collisions.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.game_physics.collisions.CollisonsGame;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.game_physics.collisions.CollisonsGame
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CollisonsGame(), config);
-	}
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(CollisonsGame(), config)
+    }
 }
