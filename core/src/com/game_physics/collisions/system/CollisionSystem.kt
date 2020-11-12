497a942f9637e5547fb5afd79e412c4473d359da
Bladejs <JBladejs@gmail.com>
2020-11-12 20:09:23 +0100

    Made collision being detected only once per collision

--- core/src/com/game_physics/collisions/system/CollisionSystem.kt
+++ core/src/com/game_physics/collisions/system/CollisionSystem.kt
@@ -4,15 +4,36 @@ import com.badlogic.gdx.utils.Array
 
 object CollisionSystem {
     private val colliders = Array<CircleCollider>()
+    private val collisions = Array<Collision>()
 
     fun add(collider: CircleCollider) = colliders.add(collider)
 
     fun remove(collider: CircleCollider) = colliders.removeValue(collider, true)
 
+
+    //TODO: Refactor - Try to make it faster using .equals()
+    private fun getCollision(collider1: CircleCollider, collider2: CircleCollider): Collision? {
+        collisions.forEach {
+            if (it.collider1 == collider1) if (it.collider2 == collider2) return it
+            else if (it.collider1 == collider2 && it.collider2 == collider1) return it
+        }
+        return null
+    }
+
     fun update() {
         for (i in 0 until colliders.size - 1) {
             for (j in i + 1 until colliders.size) {
-               if (colliders[i].collides(colliders[j])) println("COLLISION!!!")
+               if (colliders[i].collides(colliders[j])) {
+                   if (getCollision(colliders[i], colliders[j]) == null) {
+                       collisions.add(Collision(colliders[i], colliders[j]))
+                       println("COLLISION!!!")
+                   }
+               } else {
+                   val collision = getCollision(colliders[i], colliders[j])
+                   if (collision != null) {
+                       collisions.removeValue(collision, true)
+                   }
+               }
             }
         }
     }
