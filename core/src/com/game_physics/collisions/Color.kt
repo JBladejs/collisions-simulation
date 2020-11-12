72623ed9ed1d493f931de7436d82577ecb6f9d94
Bladejs <JBladejs@gmail.com>
2020-11-12 18:26:57 +0100

    Added some code from other project.

---
+++ core/src/com/game_physics/collisions/Color.kt
@@ -0,0 +1,7 @@
+package com.game_physics.collisions
+
+class Color(red: Int, green: Int, blue: Int) {
+    val red = red.toFloat() / 255.0f
+    val green = green.toFloat() / 255.0f
+    val blue = blue.toFloat() / 255.0f
+}
