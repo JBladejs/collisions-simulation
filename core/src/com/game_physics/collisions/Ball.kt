57c58bf39fc9e15e6a33b34a4662843be9424fd0
Rawaw <rafal.wawrzyniak@wir.pl>
2020-11-13 15:47:47 +0100

    remade force calculations on collision, added grabother function, started on scenarios changing with numkeyes (not working)

--- core/src/com/game_physics/collisions/Ball.kt
+++ core/src/com/game_physics/collisions/Ball.kt
@@ -7,12 +7,19 @@ import com.badlogic.gdx.math.MathUtils.sin
 import com.game_physics.collisions.system.CircleCollider
 import kotlin.math.abs
 
-class Ball(val radius: Float, private val color: Color, x: Float = 0.0f, y: Float = 0.0f, private var vx: Float = 0.0f, private var vy: Float = 0.0f) {
+class Ball(val radius: Float, private val color: Color, x: Float = 0.0f, y: Float = 0.0f, vx: Float = 0.0f, vy: Float = 0.0f) {
     var x = x
-        private set
+    private set
     var y = y
         private set
+    var vx = vx
+        private set
+    var vy = vy
+        private set
     private val collider = CircleCollider(x, y, radius)
+
+    var othervx = 0f
+    var othervy = 0f
 
     fun move(delta: Float, dt : Float) {
         x += vx * delta * dt
@@ -35,10 +42,18 @@ class Ball(val radius: Float, private val color: Color, x: Float = 0.0f, y: Floa
             val perpendicular = if (vx >= 0) if (vy >= 0) -90f else 90f else if (vy >= 0) 90f else -90f
             val angle2 = (collider.collisionAngle + perpendicular)
             println("${collider.collisionAngle} $angle1 $angle2")
-            vx = abs(vx) * (sin(angle1.toRadians()) + sin(angle2.toRadians()))
-            vy = abs(vy) * (cos(angle1.toRadians()) + cos(angle2.toRadians()))
+            //vx = abs(othervx) * (sin(angle1.toRadians()) + sin(angle2.toRadians()))
+            //vy = abs(othervy) * (cos(angle1.toRadians()) + cos(angle2.toRadians()))
+            vx = othervx * abs(sin(angle1.toRadians())) + abs(othervy) * abs(cos(angle1.toRadians()))
+            vy = othervy * (sin(angle1.toRadians())) + abs(othervx) * (cos(angle1.toRadians()))
+            //println("angle1: $angle1 sin: " + sin(angle1.toRadians()) + " cos: " + cos(angle1.toRadians()))
             collider.isColiding = false
         }
+    }
+
+    fun grabForce(forcex: Float, forcey: Float){
+        othervx = forcex
+        othervy = forcey
     }
 
     fun render(renderer: ShapeRenderer) {
--- core/src/com/game_physics/collisions/GameScreen.kt
+++ core/src/com/game_physics/collisions/GameScreen.kt
@@ -1,21 +1,53 @@
 package com.game_physics.collisions
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input.Keys.SPACE
+import com.badlogic.gdx.Input.Keys.*
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
 import com.game_physics.collisions.system.CollisionSystem
 
 
 class GameScreen(private val game: CollisionsGame) : Screen {
-    private val ball1 = Ball(30f, Color(255, 0, 0), 320f, 360f, 2f, 2f)
-    private val ball2 = Ball(30f, Color(0, 0, 255), 980f, 360f, -2f, 2f)
+    private var ball1 = Ball(30f, Color(255, 0, 0), 320f, 370f, 4f, 0f)
+    private var ball2 = Ball(30f, Color(0, 0, 255), 880f, 360f, -3f, 0f)
     private val dt = 33f
 
     private fun update(delta: Float) {
+        ball1.grabForce(ball2.vx,ball2.vy)
+        ball2.grabForce(ball1.vx,ball1.vy)
         ball1.move(delta, dt)
         ball2.move(delta, dt)
         CollisionSystem.update()
+
+        //TODO wyczyścić pierwsze piłki przed utworzeniem nowych
+        if (Gdx.input.isKeyPressed(NUMPAD_1)){
+            ball1 = Ball(30f, Color(255, 0, 0), 320f, 360f, 4f, 0f)
+            ball2 = Ball(30f, Color(0, 0, 255), 880f, 360f, 0f, 0f)
+        }
+        if (Gdx.input.isKeyPressed(NUMPAD_2)){
+            ball1 = Ball(30f, Color(255, 0, 0), 320f, 360f, 5f, 0f)
+            ball2 = Ball(30f, Color(0, 0, 255), 680f, 360f, 2f, 0f)
+        }
+        if (Gdx.input.isKeyPressed(NUMPAD_3)){
+            ball1 = Ball(30f, Color(255, 0, 0), 320f, 360f, 4f, 0f)
+            ball2 = Ball(30f, Color(0, 0, 255), 880f, 360f, -4f, 0f)
+        }
+        if (Gdx.input.isKeyPressed(NUMPAD_5)){
+            ball1 = Ball(30f, Color(255, 0, 0), 320f, 340f, 4f, 0f)
+            ball2 = Ball(30f, Color(0, 0, 255), 880f, 360f, -4f, 0f)
+        }
+        if (Gdx.input.isKeyPressed(NUMPAD_6)){
+            ball1 = Ball(30f, Color(255, 0, 0), 320f, 380f, 4f, 0f)
+            ball2 = Ball(30f, Color(0, 0, 255), 880f, 360f, 0f, 0f)
+        }
+        if (Gdx.input.isKeyPressed(NUMPAD_7)){
+            ball1 = Ball(30f, Color(255, 0, 0), 520f, 360f, 0f, 3f)
+            ball2 = Ball(30f, Color(0, 0, 255), 580f, 360f, 0f, -3f)
+        }
+        if (Gdx.input.isKeyPressed(NUMPAD_8)){
+            ball1 = Ball(30f, Color(255, 0, 0), 320f, 360f, 2f, 2f)
+            ball2 = Ball(30f, Color(0, 0, 255), 980f, 360f, -2f, 2f)
+        }
     }
 
     override fun render(delta: Float) {
